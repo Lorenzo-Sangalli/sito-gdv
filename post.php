7c280f67c4503587688cd0545d9ba338295542aa
bevocitofoni <bevocitofoni@gmail.com>
2023-02-03 16:16:09 +0100

    aldo moro e tre piloti daje roma forza via fani

---
+++ post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE hmtl>
+<html>
+
+<body>
+    <form method="post">
+    Name: <input type="text" name="fname">
+    <input type="submit">
+    </form>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $name = $_POST['fname'];
+        if (empty($name)) {
+        echo "Name is empty";
+        } else {
+        echo $name;
+        }
+    }
+?>
+</body>
+</html>
